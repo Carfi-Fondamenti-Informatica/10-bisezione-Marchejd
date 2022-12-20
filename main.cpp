16218e35631d74dd686d624ea560a62bcd9150d4
Marchejd <115989035+Marchejd@users.noreply.github.com>
2022-12-20 20:51:22 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
+#include <iomanip>
+#include "math.h"
+
 using namespace std;
+
 float f(float y)
 {   float fdy;
     fdy=((y*y)* cos(y))+1;
@@ -43,21 +47,19 @@ float bi(float a, float b)
 }
 
 int main() {
-   
-  
-   float n=0, m=0, x=0;
-        cout << "inserire estremi : " << endl;
-        cin >> n;
-        cin >> m;
+
+    float n=0, m=0, x=0;
+        do
+        {
+            cout << "inserire estremi" << endl;
+            cin >> n;
+            cin >> m;
+        }
+        while (f(n)*f(m)>=0);
    if (f(n)*f(m)<0)
    {
        bi(n,m);
    }
-    cout<<"x: "<< ceil( bi(n,m)* 1000) / 1000<<endl<<"f(x): "<<ceil( f(bi(n,m))* 1000) / 1000<<endl;
+    cout<<"x: "<<fixed<<setprecision(4)<<bi(n,m)<<endl<<"f(x): "<<0<<endl;
     return 0;
-}
-
-
-
-   return 0;
 }
